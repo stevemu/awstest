20bc15166f124c225e2b6b3e82ff4e3c59015760
Steve Mu <qmu@bu.edu>
2019-12-30 17:07:10 -0500

    add hostname

--- routes.js
+++ routes.js
@@ -1,7 +1,9 @@
+const os = require("os");
+
 function configRoutes(app) {
   // risk: if server is shutdown, the lastTextMessageDateCreatedTime is not updated when a sms comes in
   app.get("/", async (req, res) => {
-    return res.json({ message: "hello" });
+    return res.json({ message: "hello from " + os.hostname() });
   });
 }
 
