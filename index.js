46aa8b35ff02074fb3be9ff1a1b02916e117d7f1
Steve Mu <qmu@bu.edu>
2019-12-30 22:44:27 -0500

    add frontend

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ async function run() {
   const app = express();
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
+
   configRoutes(app);
+
+  app.use("/", express.static(path.resolve(__dirname, "static")));
 
   app.listen(5000, null, () => {
     console.log("server is running on " + 5000);
--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const os = require("os");
 
 function configRoutes(app) {
   // risk: if server is shutdown, the lastTextMessageDateCreatedTime is not updated when a sms comes in
-  app.get("/", async (req, res) => {
+  app.get("/host", async (req, res) => {
     return res.json({ message: "hello from " + os.hostname() });
   });
 }
