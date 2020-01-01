ae6467c03eecf7c5578523c9b357a6ce4a7e2d06
Steve Mu <qmu@bu.edu>
2020-01-01 10:54:37 -0500

    remove words

--- routes.js
+++ routes.js
@@ -10,6 +10,17 @@ function configRoutes(app) {
     let result = await req.connection.query("SELECT * from todo_t");
     return res.json(result[0]);
   });
+
+  app.post("/todos", async (req, res) => {
+    let newTodo = req.body;
+    console.log(newTodo);
+    let result = await req.connection.query(
+      "insert into todo_t set ?",
+      newTodo
+    );
+
+    return res.json(result);
+  });
 }
 
 exports.configRoutes = configRoutes;
